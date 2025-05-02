ac492321cf33873a23e7a6c6dc42f7b08835a8ea
Manuel36791 <manuelvictor36791@gmail.com>
2025-05-03 00:04:30 +0300

    feat(di): :sparkles: Configure dependency injection with environment
    Configured injectable to use different environments. This allows for different configurations for development, staging, and production.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+
+import 'di/injection.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureInjection(Environment.dev);
   runApp(const MyApp());
 }
 
