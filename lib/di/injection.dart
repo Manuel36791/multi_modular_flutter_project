a2020b3897ae64fe994ea0231317cb148313659e
Manuel36791 <manuelvictor36791@gmail.com>
2025-05-02 23:58:27 +0300

    feat(di): ✨ Implement dependency injection using GetIt and Injectable
    Configured dependency injection using GetIt and Injectable packages. This allows for easier management and testing of dependencies throughout the application.

---
+++ lib/di/injection.dart
@@ -0,0 +1,8 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:multi_modular_flutter_project/di/injection.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit()
+void configureInjection() => getIt.init();
